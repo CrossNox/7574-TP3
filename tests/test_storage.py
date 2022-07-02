9a0f91f246f71d249b0a468541f961c7423ac311
CrossNox <ijmermet@gmail.com>
2022-07-01 22:12:14 -0300

    RFC: Storage API (#18)
    * initial storage api
    
    * some tests
    
    * test
    
    * tests
    
    * add test stepping on
    
    * Update lazarus/storage/base.py
    
    Co-authored-by: Federico del Mazo <fdelmazo@fi.uba.ar>
    
    * fix
    
    * changes
    
    * bump
    
    Co-authored-by: Federico del Mazo <fdelmazo@fi.uba.ar>

---
+++ tests/test_storage.py
@@ -0,0 +1,65 @@
+# pylint: disable=redefined-outer-name
+
+import pathlib
+import tempfile
+
+import pytest
+
+from lazarus.storage.local import LocalStorage
+
+
+@pytest.fixture
+def storage():
+    with tempfile.TemporaryDirectory() as tdir:
+        yield LocalStorage(pathlib.Path(tdir))
+
+
+def test_add_element(storage):
+    storage.put("k", "v")
+    assert storage.get("k") == "v"
+
+
+def test_add_element_wtopic(storage):
+    storage.put("k", "v", topic="topic")
+    assert storage.get("k", topic="topic") == "v"
+
+
+def test_load_from_disk(storage):
+    storage.put("k", "v", topic="topic")
+    new_storage = LocalStorage.load(storage.data_dir)
+    assert new_storage.get("k", topic="topic") == "v"
+
+
+def test_add_elements(storage):
+    storage.put("k1", "v1")
+    storage.put("k2", "v2")
+    assert storage.get("k1") == "v1"
+    assert storage.get("k2") == "v2"
+
+
+def test_add_elements_wtopic(storage):
+    storage.put("k", "v1", topic="topic1")
+    storage.put("k", "v2", topic="topic2")
+    assert storage.get("k", topic="topic1") == "v1"
+    assert storage.get("k", topic="topic2") == "v2"
+
+
+def test_load_from_disk_single_topic(storage):
+    storage.put("k1", "v1", topic="topic")
+    storage.put("k2", "v2", topic="topic")
+    new_storage = LocalStorage.load(storage.data_dir)
+    assert new_storage.get("k1", topic="topic") == "v1"
+    assert new_storage.get("k2", topic="topic") == "v2"
+
+
+def test_step_on_element(storage):
+    storage.put("k1", "v1")
+    storage.put("k1", "v2")
+    assert storage.get("k1") == "v2"
+
+
+def test_load_from_disk_step_on_keys(storage):
+    storage.put("k1", "v1", topic="topic")
+    storage.put("k1", "v2", topic="topic")
+    new_storage = LocalStorage.load(storage.data_dir)
+    assert new_storage.get("k1", topic="topic") == "v2"
