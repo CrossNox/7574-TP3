647abd90531481ccfba964430a461b7ab09b39cb
CrossNox <ijmermet@gmail.com>
2022-07-03 13:21:10 -0300

    client

--- lazarus/client/cli.py
+++ lazarus/client/cli.py
@@ -1,14 +1,22 @@
 import csv
-from typing import List
-from pathlib import Path
 import multiprocessing as mp
+from pathlib import Path
+from typing import List
 
 import typer
 
 from lazarus.constants import EOS
-from lazarus.mom.message import Message
 from lazarus.mom.exchange import ConsumerType, ConsumerConfig, WorkerExchange
-from lazarus.utils import DEFAULT_PRETTY, DEFAULT_VERBOSE, get_logger, config_logging
+from lazarus.mom.message import Message
+from lazarus.utils import (
+    DEFAULT_PRETTY,
+    DEFAULT_VERBOSE,
+    get_logger,
+    parse_group,
+    exchange_name,
+    queue_in_name,
+    config_logging,
+)
 
 logger = get_logger(__name__)
 
@@ -16,28 +24,37 @@ app = typer.Typer()
 
 
 def relay_file(
-    rabbit_host: str, exchange: str, file_path: Path, queue: str, consumers: List[int]
+    rabbit_host: str, exchange: str, file_path: Path, queue: str, groups: List[str]
 ):
     # TODO: get session id
-    exch = WorkerExchange(
-        rabbit_host,
-        exchange,
-        [
-            ConsumerConfig(f"{queue}-group_{idx}-id_{j}", ConsumerType.Worker)
-            for idx, i in enumerate(consumers)
-            for j in range(i)
-        ],
-    )
+
+    groups = [parse_group(group) for group in groups]
+
+    exchanges = [
+        WorkerExchange(
+            rabbit_host,
+            exchange_name(queue, group_id),
+            [
+                ConsumerConfig(
+                    queue_in_name(queue, group_id, node_id), ConsumerType.Worker
+                )
+                for node_id in range(group_size)
+            ],
+        )
+        for group_id, group_size in groups
+    ]
 
     with open(file_path, newline="") as f:
         reader = csv.DictReader(f)
         for line in reader:
             m = {"type": "data", "session_id": 1, "data": line}  # TODO: Hardcoded
             msg = Message(data=m)
-            exch.push(msg)
+            for exch in exchanges:
+                exch.push(msg)
 
         m = {"type": EOS, "session_id": 1}  # TODO: Hardcoded
-        exch.broadcast(Message(data=m))
+        for exch in exchanges:
+            exch.broadcast(Message(data=m))
 
     exch.close()
 
@@ -51,7 +68,8 @@ def main(
     comments_exchange: str = typer.Option(
         "comments", help="Name of the comments exchange"
     ),
-    consumers: List[int] = typer.Option([1], help="Amount of consumers in each group"),
+    comments_groups: List[str] = typer.Option(..., help="<group_id>:<n>"),
+    posts_groups: List[str] = typer.Option(..., help="<group_id>:<n>"),
     verbose: int = typer.Option(
         DEFAULT_VERBOSE,
         "--verbose",
@@ -69,11 +87,11 @@ def main(
 
     pposts = mp.Process(
         target=relay_file,
-        args=(rabbit_host, posts_exchange, posts, "posts", consumers),
+        args=(rabbit_host, posts_exchange, posts, "posts", posts_groups),
     )
     pcomments = mp.Process(
         target=relay_file,
-        args=(rabbit_host, comments_exchange, comments, "comments", consumers),
+        args=(rabbit_host, comments_exchange, comments, "comments", comments_groups),
     )
 
     logger.info("Starting posts relay process")
